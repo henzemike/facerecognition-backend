0458750ee0981c3ca53f3c10341add2d01d394ce
MikeHenze <yashuachrist@gmail.com>
2019-02-08 16:25:39 -0500

    creating profile and users entry API routes

--- server.js
+++ server.js
@@ -71,26 +71,43 @@ app.post('/register', (req, res) => {
 
 });
 
+// profile page
+app.get('/profile/:id', (req, res) => {
+  const {id} = req.params;
+  let found = false;
+  // looping through users in database checking if user.id matches the id sent back
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    } 
+  });
+  // if user is not found respond with a 400 not found
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
+// update users entry count with image submit
+app.post('/image', (req, res) => {
+  const {id} = req.body;
+  let found = false;
+  // looping through users in database checking if user.id matches the id sent back
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      // increase users entries
+      user.entries++;
+      return res.json(user.entries);
+    } 
+  });
+  // if user is not found respond with a 400 not found
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
 // create port
 app.listen(3000, () => {
   console.log('app is running correctly on port 3000');
 });
-
-
-
-/*
-route route
-/ => res => GET = this is working
-
-signin route
-/signin => POST = success or fail
-
-register route
-/register => POST = new user 
-
-profile route
-/profile/:userId => GET = return user
-
-rank route
-/image => PUT = updated user object
-*/
